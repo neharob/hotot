b88f554cdf5f03607bb5e795b2daefbb5e785899
Jiahua Huang <jhuangjiahua@gmail.com>
2010-09-14 22:38:47 +0800

    bump to 1.0

---
+++ data/ui/js/ui.retweet_tabs.js
@@ -0,0 +1,26 @@
+if (typeof ui == 'undefined') var ui = {};
+ui.RetweetTabs = {
+
+current: null,
+
+init:
+function init() {
+    $('#retweets_tweet_block .tweet_tabs_btn').click(
+    function (event) {
+        if (! $(this).hasClass('selected')) {
+            // activate another sub page.
+            ui.RetweetTabs.current = $(this).attr('href');
+            var pagename = ui.RetweetTabs.current + '_sub_block';
+            $('#retweets_tweet_block .tweet_tabs_btn').not(this).removeClass('selected');
+            $(this).addClass('selected');
+            $('#retweets_tweet_block .tweet_tabs_page').not(pagename).hide();
+            $(pagename).show();
+            daemon.Updater.update_retweets();
+        }
+        return false;
+    });
+    ui.RetweetTabs.current = '#retweeted_to_me';
+    $(ui.RetweetTabs.current + '_sub_block').show();
+},
+
+};
