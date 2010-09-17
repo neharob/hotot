b00defca1836d16ba4081ecf895bdd04beb1bbbc
Jiahua Huang <jhuangjiahua@gmail.com>
2010-09-17 20:06:40 +0800

    ? gtk.main_quit() ? sys.exit(0)???? Win7 ???????

---
+++ data/ext/ext.js
@@ -0,0 +1,203 @@
+if (typeof ext == 'undefined') var ext = {};
+ext = {
+
+ADD_TWEETS_LISTENER: 0x01,
+// callback(tweets, pagename);
+
+ADD_TWEETS_LISTENER_AFTER: 0x02,
+// callback(tweets, pagename);
+
+FORM_TWEET_LISTENER: 0x03,
+// callback(tweet, pagename);
+
+FORM_TWEET_LISTENER_AFTER: 0x04,
+// callback(tweet, pagename, result_html);
+
+FORM_TWEET_TEXT_LISTENER: 0x05,
+// callback(text);
+
+FORM_TWEET_TEXT_LISTENER_AFTER: 0x06,
+// callback(text);
+
+FORM_TWEET_STATUS_INDICATOR_LISTENER: 0x07,
+// callback(text);
+
+// listeners: {listener_type: [callbacks ... ], ... };
+listeners: {},
+
+exts_info: {},
+
+exts_enabled: [], // @TODO
+
+init: 
+function init() {
+    // listeners: {listener_type: [callbacks ... ], ... };
+    for (var i = 0x01; i < 0xff; i += 0x01) {
+        ext.listeners[i] = [];
+    }
+
+    var ui_main_add_tweets = ui.Main.add_tweets;
+    ui.Main.add_tweets= function (tweet_obj, container) {
+        var cbs = ext.listeners[ext.ADD_TWEETS_LISTENER];
+        var cbs_after = ext.listeners[ext.ADD_TWEETS_LISTENER_AFTER];
+        for (var i = 0; i < cbs.length; i += 1) {
+            cbs[i](tweet_obj, container);
+        }
+        
+        ret = ui_main_add_tweets(tweet_obj, container);
+
+        for (var i = 0; i < cbs_after.length; i += 1) {
+            cbs_after[i](tweet_obj, container);
+        }
+        return ret;
+    };
+
+    var ui_template_form_tweet = ui.Template.form_tweet;
+    ui.Template.form_tweet = function (tweet_obj, pagename) {
+        var cbs = ext.listeners[ext.FORM_TWEET_LISTENER];
+        var cbs_after = ext.listeners[ext.FORM_TWEET_LISTENER_AFTER];
+        for (var i = 0; i < cbs.length; i += 1) {
+            cbs[i](tweet_obj, pagename);
+        }
+        
+        result_html = ui_template_form_tweet(tweet_obj, pagename);
+
+        for (var i = 0; i < cbs_after.length; i += 1) {
+            result_html = cbs_after[i](tweet_obj, pagename, result_html);
+        }
+        return result_html;
+    };
+
+    var ui_template_form_status_indicators
+        = ui.Template.form_status_indicators;
+    ui.Template.form_status_indicators = function (tweet, text) {
+        var cbs = ext.listeners[ext.FORM_TWEET_STATUS_INDICATOR_LISTENER];
+        text = '';
+        for (var i = 0; i < cbs.length; i += 1) {
+            text = cbs[i](tweet, text);
+        }
+        return text;
+    };
+
+    var ui_template_form_text = ui.Template.form_text;
+    ui.Template.form_text = function (text) {
+        var cbs = ext.listeners[ext.FORM_TWEET_TEXT_LISTENER];
+        var cbs_after = ext.listeners[ext.FORM_TWEET_TEXT_LISTENER_AFTER];
+        for (var i = 0; i < cbs.length; i += 1) {
+            cbs[i](text);
+        }
+        
+        text = ui_template_form_text(text);
+
+        for (var i = 0; i < cbs_after.length; i += 1) {
+            text = cbs_after[i](text);
+        }
+        return text;
+    };
+
+},
+
+init_exts:
+function init_exts() {
+    for (var key in ext) {
+        // Extension package MUST be Capital
+        // and MUST have two methods named 'load' and 'unload'
+        if (65 <= key.charCodeAt(0) 
+            && key.charCodeAt(0) <= 90
+            && typeof  ext[key].load != 'undefined' 
+            && typeof  ext[key].unload != 'undefined') {
+
+            var extension = ext[key];
+
+            if (typeof extension.icon == 'undefined') {
+                icon = 'imgs/ic64_exts.png';
+            } else {
+                icon = '../ext/' + extension.id + '/' + extension.icon;
+            }
+
+            ext.exts_info[extension.id] = {
+                  name: extension.name
+                , description: extension.description
+                , version: extension.version
+                , author: extension.author
+                , url: extension.url
+                , icon: icon
+                , extension: extension
+            };
+
+            // @TODO Issue 31
+            if (ext.exts_enabled.indexOf(extension.id) != -1) {
+                utility.Console.out('[i]Load Extension: ' + extension.name);
+                extension.load();
+                ext.exts_info[extension.id]['enable'] = true;
+            } else {
+                ext.exts_info[extension.id]['enable'] = false;
+            }
+            /*
+            extension.load();
+            */
+        }
+    }
+},
+
+load_exts:
+function load_exts(exts) {
+    procs = [];
+    var _load = function (idx) {
+        var path = exts[i];
+        procs.push(function () {
+            $.getScript(path,
+            function () {
+                utility.Console.out('[i]Load Extension: ' + path);
+                $(window).dequeue('_load_exts');
+            });
+        });    
+    };
+
+    for (var i = 0; i < exts.length; i += 1) {
+        _load(i)
+    }
+    procs.push(function () { ext.init_exts(); });
+    $(window).queue('_load_exts', procs);
+    $(window).dequeue('_load_exts');
+},
+
+notify:
+function notify(type){
+    // #TODO
+},
+
+register_listener:
+function register_listener(type, callback) {
+    if (ext.listeners.hasOwnProperty(type)) {
+        if (! (callback in ext.listeners[type])) {
+            ext.listeners[type].push(callback);
+        }
+    }
+},
+
+unregister_listener:
+function unregister_listener(type, callback) {
+    if (ext.listeners.hasOwnProperty(type)) {
+        var idx = ext.listeners[type].indexOf(callback);
+        if (idx != -1) {
+            ext.listeners[type].splice(idx, 1);
+        }
+    }
+},
+
+add_toolbar_button:
+function add_toolbar_button(id, icon, title, callback) {
+    $('#ctrl_panel').prepend('<li><a id="'+id+'" class="panel_btn"  style="background-image:url('+icon+'); background-repeat: no-repeat;" href="javascript:void(0);" title="'+title+'"></a></li>');
+    $('#'+id).click(callback);
+},
+
+remove_toolbar_button:
+function remove_toolbar_button(id) {
+    $('#'+id).unbind('click');
+    var li = $('#'+id).parent();
+    li.remove();
+},
+
+
+};
---
+++ hotot/hotot.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+'''Hotot
+@author: U{Shellex Wei <5h3ll3x@gmail.com>}
+@license: LGPLv3+
+'''
+
+__version__ = '0.9.1'
+__codename__ = 'Ada'
+
+import gtk
+import gobject
+import view
+import config
+import agent
+import keybinder
+import db
+
+try:
+    import appindicator
+except ImportError:
+    HAS_INDICATOR = False
+else:
+    HAS_INDICATOR = True
+
+class MainWindow:
+    def __init__(self):
+        self.build_gui()
+        self.build_inputw()
+        if not HAS_INDICATOR:
+            self.create_trayicon()
+        self.init_hotkey()
+        pass
+
+    def build_gui(self):
+        self.window = gtk.Window()
+        gtk.window_set_default_icon_from_file(
+            config.get_ui_object('imgs/ic64_hotot.png'))
+        self.window.set_icon_from_file(
+            config.get_ui_object('imgs/ic64_hotot.png'))
+        self.window.set_default_size(750, 550)
+        self.window.set_title('Hotot')
+        self.window.set_position(gtk.WIN_POS_CENTER)
+
+        vbox = gtk.VBox()
+        scrollw = gtk.ScrolledWindow()
+        self.webv = view.MainView()
+
+        agent.view = self.webv
+
+        scrollw.add(self.webv)
+        vbox.pack_start(scrollw)
+        vbox.show_all()
+        self.window.add(vbox)
+
+        self.menu_tray = gtk.Menu()
+        mitem_resume = gtk.MenuItem('Resume/Active')
+        mitem_resume.connect('activate', self.on_mitem_resume_activate);
+        self.menu_tray.append(mitem_resume)
+        mitem_prefs = gtk.MenuItem('Preferences')
+        mitem_prefs.connect('activate', self.on_mitem_prefs_activate);
+        self.menu_tray.append(mitem_prefs)
+        mitem_about = gtk.MenuItem('About')
+        mitem_about.connect('activate', self.on_mitem_about_activate);
+        self.menu_tray.append(mitem_about)
+        mitem_quit = gtk.MenuItem('Quit')
+        mitem_quit.connect('activate', self.on_mitem_quit_activate);
+        self.menu_tray.append(mitem_quit)
+
+        self.menu_tray.show_all()
+
+        self.window.show()
+        self.window.connect("delete-event", gtk.Widget.hide_on_delete)
+        pass
+    
+    def build_inputw(self):
+        # input window
+        self.inputw = gtk.Window()
+        self.inputw.set_position(gtk.WIN_POS_CENTER)
+        self.inputw.set_title('What\'s happening?')
+        hbox = gtk.HBox()
+
+        self.tbox_status = gtk.Entry()
+        self.tbox_status.connect('changed', self.on_tbox_status_changed)
+        self.tbox_status.connect('key-release-event'
+            , self.on_tbox_status_key_released)
+        hbox.pack_start(self.tbox_status)
+
+        self.btn_update = gtk.Button('Update')
+        self.btn_update.connect('clicked', self.on_btn_update_clicked) 
+        hbox.pack_start(self.btn_update)
+
+        hbox.show_all() 
+        self.inputw.add(hbox)
+        self.inputw.connect('delete-event', gtk.Widget.hide_on_delete)
+        pass
+
+    def on_btn_update_clicked(self, btn):
+        if (self.tbox_status.get_text_length() <= 140):
+            agent.update_status(self.tbox_status.get_text())
+            self.tbox_status.set_text('')
+            self.inputw.hide()
+        pass
+
+    def on_tbox_status_changed(self, entry):
+        if (self.tbox_status.get_text_length() <= 140):
+            entry.modify_base(gtk.STATE_NORMAL, gtk.gdk.Color('#fff'))
+        else:
+            entry.modify_base(gtk.STATE_NORMAL, gtk.gdk.Color('#f00'))
+        pass
+    
+    def on_tbox_status_key_released(self, entry, event):
+        if event.keyval == gtk.keysyms.Return:
+            self.btn_update.clicked();
+            entry.stop_emission('insert-text')
+        pass
+
+    def on_mitem_resume_activate(self, item):
+        self.window.present()
+        pass
+
+    def on_mitem_prefs_activate(self, item):
+        agent.execute_script('ui.DialogHelper.open(ui.PrefsDlg);');
+        self.window.present()
+        pass
+
+    def on_mitem_about_activate(self, item):
+        agent.execute_script('ui.DialogHelper.open(ui.AboutDlg);');
+        self.window.present()
+        pass
+
+    def on_mitem_quit_activate(self, item):
+        self.quit()
+        pass
+
+    def quit(self, *args):
+        db.dump_screen_name(db.get_screen_name(self.webv))
+
+        gtk.gdk.threads_leave()
+        self.window.destroy()
+        gtk.main_quit() 
+        import sys
+        sys.exit(0)
+        pass
+        
+    def init_hotkey(self):
+        keybinder.bind(config.shortcut_summon_hotot, self.on_hotkey_compose)
+        pass
+
+    def create_trayicon(self):
+        """ 
+        Create status icon and connect signals
+        """
+        self.trayicon = gtk.StatusIcon()
+        self.trayicon.connect('activate', self.on_trayicon_activate)
+        self.trayicon.connect('popup-menu', self.on_trayicon_popup_menu)
+        self.trayicon.set_tooltip('Hotot: Click to Active.')
+        self.trayicon.set_from_file(
+            config.get_ui_object('imgs/ic64_hotot.png'))
+        self.trayicon.set_visible(True)
+        pass
+
+    def on_trayicon_activate(self, icon):
+        if self.window.is_active():
+            self.window.hide()
+        else:
+            self.window.present()
+        pass
+
+    def on_trayicon_popup_menu(self, icon, button, activate_time):
+        self.menu_tray.popup(None, None
+            , None, button=button
+            , activate_time=activate_time)
+        pass
+
+    def on_hotkey_compose(self):
+        if config.use_native_input:
+            self.inputw.present()
+            self.tbox_status.grab_focus()
+        else:
+            self.window.present()
+        pass
+
+def main():
+    gtk.gdk.threads_init()
+    config.loads();
+    try:
+        import dl
+        libc = dl.open('/lib/libc.so.6')
+        libc.call('prctl', 15, 'hotot', 0, 0, 0)
+    except:
+        pass
+    agent.init_notify()
+    app = MainWindow()
+    agent.app = app
+    if HAS_INDICATOR:
+        #TODO the icon is only work when installed to /usr/share/icons/hicolor/
+        indicator = appindicator.Indicator('hotot',
+                                           'hotot',
+                                           appindicator.CATEGORY_COMMUNICATIONS)
+        indicator.set_status(appindicator.STATUS_ACTIVE)
+        indicator.set_attention_icon(config.get_ui_object('imgs/ic64_hotot.png'))
+        indicator.set_menu(app.menu_tray)
+        pass
+    gtk.gdk.threads_enter()
+    gtk.main()
+    gtk.gdk.threads_leave()
+
+if __name__ == '__main__':
+    main()
+
